66c7800d88d31e581e8c077b8a394fd74c7efa4a
Oluwasegun Dada <oluwasegunoluwafemidada@gmail.com>
2026-08-15 23:20:33 +0100

    add condition to pin change

--- apps/web/src/app/app/profile/page.tsx
+++ apps/web/src/app/app/profile/page.tsx
@@ -744,7 +744,7 @@ export default function ProfilePage() {
               <form
                 onSubmit={async (e) => {
                   e.preventDefault();
-                  if (newPin !== confirmPin) {
+                  if (newPin !== confirmPin && currentPin.trim() !== "currentPin") {
                   setFeedbackModal({
                     title: "Pin mismatch",
                     message: "The new PINs do not match.",
