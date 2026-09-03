d3403fc2c09b0ceec23dc950ebdc8b9f609c11df
Oluwasegun Dada <oluwasegunoluwafemidada@gmail.com>
2026-09-03 13:06:50 +0100

    wallet reconciliation integrity

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -1,22 +1,20 @@
 // Path: apps/api/src/modules/payments/payments.module.ts
 import { Module, forwardRef } from "@nestjs/common";
-import { ScheduleModule } from "@nestjs/schedule";
 import { PrismaModule } from "../../infra/prisma/prisma.module";
 import { AdminModule } from "../../admin/admin.module";
 import { WalletModule } from "../wallet/wallet.module";
 import { NotificationsModule } from "../notifications/notifications.module";
 import { PaymentsController } from "./payments.controller";
 import { PaymentsService } from "./payments.service";
-import { ReconciliationService } from "./reconciliation.service";
 import { PAYMENT_PROVIDER } from "./payments.constants";
 import { MonnifyHttpProvider } from "./monnify/monnify.http.provider";
 import { MonnifyWebhookController } from "./monnify/monnify.webhook.controller";
 import { JobPaymentsModule } from "../job-payments/job-payments.module";
+
 @Module({
   imports: [
     PrismaModule,
     NotificationsModule,
-    ScheduleModule,
     forwardRef(() => AdminModule),
     forwardRef(() => WalletModule),
     JobPaymentsModule,
@@ -29,7 +27,6 @@ import { JobPaymentsModule } from "../job-payments/job-payments.module";
 
   providers: [
     PaymentsService,
-    ReconciliationService,
     {
       provide: PAYMENT_PROVIDER,
       useClass: MonnifyHttpProvider,
